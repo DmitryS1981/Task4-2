5d8122712627d4f64a3f54dad33fdf64f9edf584
DmitryS1981 <dir2@odis.su>
2025-11-03 20:33:51 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,42 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.*;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+interface PhoneBook {
+    void add(String lastName, String phoneNumber);
+    List<String> get(String lastName);
+}
+class SimplePhoneBook implements PhoneBook {
+    private Map<String, List<String>> contacts;
+
+    public SimplePhoneBook() {
+        this.contacts = new HashMap<>();
+    }
+    @Override
+    public void add(String lastName, String phoneNumber) {
+        contacts.putIfAbsent(lastName, new ArrayList<>());
+        contacts.get(lastName).add(phoneNumber);
+    }
+    @Override
+    public List<String> get(String lastName) {
+        return contacts.getOrDefault(lastName, new ArrayList<>());
+    }
+    public void printAllContacts() {
+        for (Map.Entry<String, List<String>> entry : contacts.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        PhoneBook phoneBook = new SimplePhoneBook();
+
+        phoneBook.add("Иванов", "+7-911-456-7890");
+        phoneBook.add("Петров", "+7-911-654-3210");
+        phoneBook.add("Иванов", "+7-911-222-3333");
+
+        System.out.println("\nВесь справочник: ");
+        if (phoneBook instanceof SimplePhoneBook) {
+            ((SimplePhoneBook) phoneBook).printAllContacts();
+        }
+    }
 }
